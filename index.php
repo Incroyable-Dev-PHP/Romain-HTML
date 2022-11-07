ac985a003b23ef8a640990b55f9fe7d5dd5361ae
Romainlens <r.monier0@laposte.net>
2022-11-07 11:09:13 +0100

    php

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+    <head>
+        <meta charset="utf-8">
+        <title>Accueil</title>
+        <link  href="./style.css" rel="stylesheet" >
+    </head>
+
+    <body>
+        <?php include("includes/header.html");?>
+
+        <nav>
+            <ul>
+                <li><a href="./index.html">Accueil</a></li>
+                <li><a href="./about-me.html">À propos de moi</a></li>
+                <li><a href="./my-dreams.html">Mes rêves d'apprenant</a></li>
+                <li><a href="./my-passions.html">Mes passions</a></li>
+                <li><a href="./cv-fr.pdf" target="_blank">Mon CV</a></li>
+            </ul>
+        </nav>
+
+        <section>
+            <h1>Moi</h1>
+            <a href="./img/Romain.png"><img src="./img/Romain.png" alt="image" id="Romain-mini"/></a>
+        
+            <p><strong>Romain Monier</strong></p>
+        <p>Aprés avoir fait quelques formations en bureautique,en 2020,J'ai fait une formation dans le numérique qui ma permis d'avoir les base en HTML,CSS et Javascript,que j'ai développeur aussi en 2022 a la Nurserie de Lomme et dans la formation Developpeur Front-End avec Simplon ou j'ai pu voir en plus du React</p>
+            <p>Je me suis trouver une impétance pour la programmation c'est pour cela,que jaimerais devenir Developpeur Web/Web Mobile</p>
+        </section>
+
+        <footer>
+            <p>&copy; copyright Romain Monier– toute reproduction est strictement interdite</p>
+        </footer>
+    </body>
+</html>
